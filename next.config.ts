3f70def82d0ff82575ec9aaf7f75653da8b82f03
myphan <haimyphan125@gmail.com>
2025-03-18 17:12:08 +0700

    NextConfig domains images

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ["res.cloudinary.com"],
+  },
   /* config options here */
 };
 
